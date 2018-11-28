2ddd1336327cf8e8147a7c3109a0584f8228d9ed
EpicCrypto <43456229+EpicCrypto@users.noreply.github.com>
2018-11-28 02:18:57 +0200

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -8,5 +8,5 @@ wget https://github.com/EpicCrypto/Epic/releases/download/2.1.0/epic-2.1.0-x86_6
 tar -xzf epic-2.1.0-x86_64-linux-gnu.tar.gz
 rm -rf epic-2.1.0-x86_64-linux-gnu.tar.gz
 ./epicd -daemon
-sleep 5
+sleep 30
 ./epic-cli getinfo
