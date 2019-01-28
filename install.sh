9872ef0a35724246ae1d26d0cf862de936be3a74
EpicCrypto <43456229+EpicCrypto@users.noreply.github.com>
2019-01-28 03:37:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,8 +83,8 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 clear
 
 # Set these to change the version of epic to install
-TARBALLURL="https://github.com/EpicCrypto/Epic/releases/download/2.2.0/epic-2.2.0-x86_64-linux-gnu.tar.gz"
-TARBALLNAME="epic-2.2.0-x86_64-linux-gnu.tar.gz"
+TARBALLURL="https://github.com/EpicCrypto/Epic/releases/download/2.2.1/epic-2.2.1-x86_64-linux-gnu.tar.gz"
+TARBALLNAME="epic-2.2.1-x86_64-linux-gnu.tar.gz"
 BOOTSTRAPURL=""
 BOOTSTRAPARCHIVE=""
 BWKVERSION="1.0.0"
